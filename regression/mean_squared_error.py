212b2b9d4a18a7e3f21551083dcc832f14885810
averma10 <anuj.vermacom@gmail.com>
2020-01-11 23:53:41 -0700

    complete

--- regression/mean_squared_error.py
+++ regression/mean_squared_error.py
@@ -2,7 +2,35 @@
 import numpy as np
 
 
-def mean_squared_error(actual: np., predicted):
+def squared_error(actual: np.array, predicted: np.array) -> np.array:
+    """
+    calculate the squared error.
+    Args:
+        actual: actual labels
+        predicted: predicted labels
+
+    Returns: Squared Error
+
+    """
+
+    return (actual - predicted)**2
+
+
+def sum_squared_error(actual: np.array, predicted: np.array) -> int:
+    """
+    calculate the sum of squared error.
+    Args:
+        actual: actual labels
+        predicted: predicted labels
+
+    Returns: sum of squared error
+
+    """
+
+    return np.sum(squared_error(actual, predicted))
+
+
+def mean_squared_error(actual: np.array, predicted: np.array) -> float:
     """
     calculate mean squared error.
     Args:
@@ -13,3 +41,4 @@ def mean_squared_error(actual: np., predicted):
         Mean Squared Error from provided params
 
     """
+    return np.mean(squared_error(actual, predicted))
