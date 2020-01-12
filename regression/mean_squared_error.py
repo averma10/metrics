e1312c8865e392c010fa39f0584930a26de1f660
averma10 <anuj.vermacom@gmail.com>
2020-01-11 23:00:41 -0700

    added mean squared error

---
+++ regression/mean_squared_error.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def mean_squared_error(actual: np., predicted):
+    """
+    calculate mean squared error.
+    Args:
+        actual: actual labels
+        predicted: predicted labels
+
+    Returns:
+        Mean Squared Error from provided params
+
+    """
